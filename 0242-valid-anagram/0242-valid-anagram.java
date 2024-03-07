60fa319f3efcc9ffbf880522e48683f33ef08797
vivekadapa <92251450+vivekadapa@users.noreply.github.com>
2024-03-07 17:29:01 +0530

    Time: 26 ms (7.36%), Space: 44.9 MB (9.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,42 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-            int sArr[] = new int[128];
-            int tArr[] = new int[128];
-            if(s.length() != t.length()){
+        Map<Character,Integer> sMap = new HashMap<>();
+        Map<Character,Integer> tMap = new HashMap<>();
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(sMap.containsKey(s.charAt(i))){
+                sMap.put(s.charAt(i),sMap.get(s.charAt(i)) + 1);
+            }
+            else{
+                sMap.put(s.charAt(i),1);
+            }
+        }
+        System.out.println(sMap.toString());
+        for(int i=0;i<t.length();i++){
+            if(tMap.containsKey(t.charAt(i))){
+                tMap.put(t.charAt(i),tMap.get(t.charAt(i)) + 1);
+            }
+            else{
+                tMap.put(t.charAt(i),1);
+            }
+        }
+        System.out.println(tMap.toString());
+        
+        for(int i=0;i<s.length();i++){
+            if(!sMap.containsKey(s.charAt(i)) || !tMap.containsKey(s.charAt(i))){
                 return false;
             }
-            for(int i=0;i<s.length();i++){
-                sArr[s.charAt(i)]++;
-                tArr[t.charAt(i)]++;
+            int a = tMap.get(s.charAt(i));
+            int b = sMap.get(s.charAt(i));
+            if(a != b){
+                return false;
             }
-            
-            for(int i=0;i<s.length();i++){
-                if(sArr[s.charAt(i)] != tArr[s.charAt(i)]){
-                    return false;
-                }
-            }
+        }
         return true;
+        
     }
 }
