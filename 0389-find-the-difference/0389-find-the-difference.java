91fb60975890c72558066fa7ac7e034b9749fc92
vivekadapa <92251450+vivekadapa@users.noreply.github.com>
2024-05-15 13:12:59 +0530

    Time: 2 ms (73.69%), Space: 41.5 MB (82.26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,20 +1,15 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-       HashMap<Character,Integer> smap = new HashMap<>();
-       HashMap<Character,Integer> tmap = new HashMap<>();
-       int i = 0;
-       while(i < s.length()){
-           smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0) + 1);
-           tmap.put(t.charAt(i),tmap.getOrDefault(t.charAt(i),0) + 1);
-           i++;
-       }
-       tmap.put(t.charAt(t.length()-1),tmap.getOrDefault(t.charAt(t.length()-1),0) + 1);
+        int sSum = 0;
+        int tSum = 0;
+        for(int i=0;i<t.length();i++){
+            if(i<s.length()){
+                sSum = sSum +  s.charAt(i);
+            }
+            tSum = tSum + t.charAt(i);
+        }
         
-       for(int j=0;j<t.length();j++){
-           if(smap.get(t.charAt(j)) != tmap.get(t.charAt(j))){
-               return t.charAt(j);
-           }
-       }
-       return 'a';
+        char c = (char)(tSum - sSum);
+        return c;
     }
 }
